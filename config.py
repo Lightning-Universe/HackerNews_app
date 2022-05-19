b7d9c42aa8c8ddc5f70fdb832400450b7f834135
Kaushik B <kaushikbokka@gmail.com>
2022-05-19 17:49:09 +0530

    Add models & config

---
+++ config.py
@@ -0,0 +1,28 @@
+import os
+
+
+class BaseConfig:
+    """General configuration."""
+
+    num_epochs = 2
+    num_batches_show_loss = 100  # Number of batchs to show loss
+    # Number of batchs to check metrics on validation dataset
+    num_batches_validate = 1000
+    batch_size = 128
+    learning_rate = 0.0001
+    num_workers = 4  # Number of workers for data loading
+    num_clicked_news_a_user = 50  # Number of sampled click history for each user
+    num_words_title = 20
+    word_freq_threshold = 1
+    entity_freq_threshold = 2
+    entity_confidence_threshold = 0.5
+    negative_sampling_ratio = 2  # K
+    dropout_probability = 0.2
+    word_embedding_dim = 300
+    # Modify the following only if you use another dataset
+    entity_embedding_dim = 100
+    # For additive attention
+    query_vector_dim = 200
+    # For CNN
+    num_filters = 300
+    window_size = 3
---
+++ models/story_encoder.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from model.general.attention.additive import AdditiveAttention
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+class StoryEncoder(torch.nn.Module):
+    def __init__(self, config, pretrained_word_embedding):
+        super().__init__()
+        self.config = config
+        if pretrained_word_embedding is None:
+            self.word_embedding = nn.Embedding(
+                config.num_words, config.word_embedding_dim, padding_idx=0
+            )
+        else:
+            self.word_embedding = nn.Embedding.from_pretrained(
+                pretrained_word_embedding, freeze=False, padding_idx=0
+            )
+        assert config.window_size >= 1 and config.window_size % 2 == 1
+        self.title_CNN = nn.Conv2d(
+            1,
+            config.num_filters,
+            (config.window_size, config.word_embedding_dim),
+            padding=(int((config.window_size - 1) / 2), 0),
+        )
+        self.title_attention = AdditiveAttention(
+            config.query_vector_dim, config.num_filters
+        )
+
+    def forward(self, stories):
+        """
+        Args:
+            stories:
+                {
+                    "title": batch_size * num_words_title
+                }
+        Returns:
+            (shape) batch_size, num_filters
+        """
+        # batch_size, num_words_title, word_embedding_dim
+        title_vector = F.dropout(
+            self.word_embedding(stories["title"].to(device)),
+            p=self.config.dropout_probability,
+            training=self.training,
+        )
+        # batch_size, num_filters, num_words_title
+        convoluted_title_vector = self.title_CNN(title_vector.unsqueeze(dim=1)).squeeze(
+            dim=3
+        )
+        # batch_size, num_filters, num_words_title
+        activated_title_vector = F.dropout(
+            F.relu(convoluted_title_vector),
+            p=self.config.dropout_probability,
+            training=self.training,
+        )
+        # batch_size, num_filters
+        weighted_title_vector = self.title_attention(
+            activated_title_vector.transpose(1, 2)
+        )
+
+        return weighted_title_vector
