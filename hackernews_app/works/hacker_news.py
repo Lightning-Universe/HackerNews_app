601ff337dce0f906f94f26522c151794063aeda5
Eric Chea <5069128+EricChea@users.noreply.github.com>
2022-05-23 12:49:06 -0400

    Update hackernews_app/works/hacker_news.py
    Co-authored-by: Marc Ferradou <zippeurfou@gmail.com>

--- hackernews_app/works/hacker_news.py
+++ hackernews_app/works/hacker_news.py
@@ -106,8 +106,8 @@ class HackerNewsGetItem(L.LightningWork):
         ) -> Callable[[pubsub_v1.publisher.futures.Future], None]:
             def callback(publish_future: pubsub_v1.publisher.futures.Future) -> None:
                 try:
-                    # Wait 60 seconds for the publish call to succeed.
-                    print(publish_future.result(timeout=60))
+                    # Wait self.publish_timeout seconds for the publish call to succeed.
+                    print(publish_future.result(timeout=self.publish_timeout))
                 except futures.TimeoutError:
                     print(f"Publishing {data} timed out.")
 
