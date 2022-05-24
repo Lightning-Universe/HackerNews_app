229752d40eca24a1e16a2981f364e83efd63b0c4
Aniket Maurya <theaniketmaurya@gmail.com>
2022-05-24 19:16:05 +0530

    update

--- hackernews_app/ui/home.py
+++ hackernews_app/ui/home.py
@@ -1,32 +1,39 @@
+from typing import Dict, List
+
 import lightning as L
 import pandas as pd
 import requests
 import streamlit as st
 from lightning.utilities.state import AppState
 
-from config import HACKERNEWS_API
+from config import HACKERNEWS_TOPICS_DATA, HACKERNEWS_USER_DATA
 
 
 def user_welcome(state: AppState):
-    dummy_users = [None, "Marc", "Aniket", "Rohit", "Kaushik"]
+    users = [""] + list(requests.get(HACKERNEWS_USER_DATA).json().keys())
     if not state.username:
         st.title("Welcome to HackerRec!")
-        state.username = st.selectbox("Select user", dummy_users)
+        state.username = st.selectbox("Select user", users)
     else:
         st.title(f"Hey {state.username}, Here are your recommendations!")
 
 
-def recommendations(state: AppState):
-    response = requests.get(HACKERNEWS_API).json()
+def get_story_data(username=None):
+    response = requests.get(HACKERNEWS_TOPICS_DATA).json()
     titles, topics, created_dates = [], [], []
-    for id, story_data in response.items():
-        title = story_data["orig_title"]
-        url = story_data["url"]
-        topic = story_data["topic"]
-        created_on = "24th May 2022"  # TODO: fetch date here
-        titles.append(f"<a href='{url}'>{title}</a>")
-        topics.append(topic)
-        created_dates.append(created_on)
+    user_data = None
+    if username:
+        user_data = get_user_data(username)
+
+    for story_id, story_data in response.items():
+        if not username or story_id in user_data:
+            title = story_data["orig_title"]
+            url = story_data["url"]
+            topic = story_data["topic"]
+            created_on = "24th May 2022"  # TODO: fetch date here
+            titles.append(f"<a href='{url}'>{title}</a>")
+            topics.append(topic)
+            created_dates.append(created_on)
 
     data = {
         "Story Title": titles,
@@ -34,6 +41,17 @@ def recommendations(state: AppState):
         "Created on": created_dates,
     }
     df = pd.DataFrame(data)
+    return df
+
+
+def get_user_data(username: str) -> Dict[str, float]:
+    users = requests.get(HACKERNEWS_USER_DATA).json()
+    user: List[Dict[str, float]] = users[username]
+    return {list(e.keys())[0]: list(e.values())[0] for e in user}
+
+
+def recommendations(state: AppState):
+    df = get_story_data(state.username)
 
     hide_table_row_index = """
                 <style>
