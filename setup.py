54653417457e7f6a3a226e173d8710aa8fcdbea8
Aniket Maurya <theaniketmaurya@gmail.com>
2022-05-26 15:40:05 +0530

    add installation test (#12)
    * add installation test
    
    * fix
    
    * fix
    
    * update testing
    
    * bump lightning
    
    * bump lightning
    
    * update
    
    * rename
    
    * update
    
    * update testing

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="hackernews_app",
+    version="0.0.1",
+    description="Personalized HackerNews stories for you!",
+    author="Grid.ai",
+    packages=setuptools.find_packages(),
+    include_package_data=False,
+    python_requires=">=3.8",
+)
