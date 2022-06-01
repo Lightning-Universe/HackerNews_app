c34aa3af4ace8b1d379cf073f5459a3dff769fc0
rohitgr7 <rohitgr1998@gmail.com>
2022-06-01 21:11:58 +0530

    run works in parallel

--- hackernews_app/works/topic_classification.py
+++ hackernews_app/works/topic_classification.py
@@ -5,12 +5,10 @@ from ml.topic_classification.inference import predict as topic_predict
 
 class TopicClassification(L.LightningWork):
     def __init__(self, weights_path):
-        super().__init__()
+        super().__init__(parallel=True)
         # TODO: provide proper weights_path (@rohitgr7)
         self.weights_path = "ml/topic_classification/epoch=4-step=25110.ckpt"
-        self.topics = None
 
     def run(self, stories):
         topics = topic_predict([story["title"] for story in stories], self.weights_path)
         topics = [{"id": story["id"], "topic": topic} for story, topic in zip(stories, topics)]
-        self.topics = L.storage.Payload(topics)
