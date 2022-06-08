b2247ceb155c4976689791af6be60e3046cc2124
rohitgr7 <rohitgr1998@gmail.com>
2022-06-08 19:43:57 +0530

    add constants

---
+++ hackernews_app/api/constants.py
@@ -0,0 +1,8 @@
+HACKERNEWS_BASEURL = "https://hacker-news.firebaseio.com/v0"
+
+HACKERNEWS_ITEMS_ENDPOINT = "/item/{id}.json"
+HACKERNEWS_USERS_ENDPOINT = "/user/{id}.json"
+HACKERNEWS_MAX_ITEM_ENDPOINT = "/maxitem.json"
+HACKERNEWS_TOP_STORIES_ENDPOINT = "/topstories.json"
+HACKERNEWS_NEW_STORIES_ENDPOINT = "/newstories.json"
+HACKERNEWS_BEST_STORIES_ENDPOINT = "/beststories.json"
